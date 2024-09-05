ea6df79675e41888b732cb6ef680b8bd1857f377
pablo-vieira-59 <pablo.vieira.andrade@gmail.com>
2024-09-05 17:37:58 -0300

    Updated gitignore

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -2,8 +2,9 @@ import { timeout } from "rxjs";
 
 export const environment = {
     production: false,
-    backendUrl: 'http://localhost:7860/',
+    //backendUrl: 'http://localhost:7860/',
+    backendUrl: 'https://api.ocorp.com.br:8443/',
     ambient: "Local",
     timeoutValue: 3*60*1000,
-    auth:`---`
+    auth:`cGFibG8udmllaXJhOlQ1RFhMd3RtTm05M2hwNVI=`
 };
